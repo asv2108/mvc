2bd45d9028a3050743225cd296022a4b54285c80
Alexey Vasiliev home <asv2108@gmail.com>
2017-08-17 16:31:20 +0300

    Created a skeleton of application

---
+++ controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+class IndexController
+{
+    public function actionIndex()
+    {
+        return "actionIndex";
+    }
+
+    public function actionUser()
+    {
+        return "actionUsers";
+
+    }
+
+    public function actionAdvertisement()
+    {
+        
+        return "actionAdvertisement";
+    }
+}
+
---
+++ views/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+?>
+
+<h1>Here we get resources from IndexController indexAction. </h1>
